c50b9c5dad77c248753023bb65a27bec584c1e12
Arpit <arpit7soni@gmail.com>
2026-05-20 10:54:51 +0000

    wrote some post thing

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ const app = express();
 
 app.use(express.json())
 
-app.get("/",()=>{
+const notes = [] // this is bad (in memory db-storing in a variable like this), later we will use mongodb,postgres and mysql for storing databases
+
+//create a note //client give the note in json body 
+app.post("/notes",(req,res)=>{
+    const new_note= req.body.note;
+    // stored the note that came from the client
+    notes.push(new_note); 
+    // lets send user a msg :)
+    
+    res.json({
+        msg:"Done!"
+    })
+
+
+})
+
+
+//get all my notes 
+app.get("/",(req,res)=>{
     
 })
 
